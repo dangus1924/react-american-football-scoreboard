c8d7deae0e0fda7e4acbefd21c25a4eabe1a47ef
dangus1924 <51095136+dangus1924@users.noreply.github.com>
2019-08-21 20:53:23 -0400

    got the timer working

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import ReactDom from "react-dom";
+import Countdown from "react-countdown-now"
 
+
+//
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [Home, hScore] = useState(0);
   const [Away, aScore] = useState(0);
+  
 
   return (
     <div className="container">
@@ -19,7 +24,7 @@ function App() {
 
             <div className="home__score">{Home}</div>
           </div>
-          <div className="timer">00:03</div>
+          <div className="timer"> <Countdown date={Date.now() + 300000}> </Countdown></div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
             <div className="away__score">{Away}</div>
@@ -32,11 +37,15 @@ function App() {
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={() => hScore(Home + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => hScore(Home + 3)}>Home Field Goal</button>
+          <button className="reset_fieldGoal" onClick={() => hScore(0)}>Score reset</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={() => aScore(Away + 7)}>Away Touchdown</button>
           <button className="awayButtons__fieldGoal" onClick={() => aScore(Away + 3)}>Away Field Goal</button>
+          <button className="reset_touchdown" onClick={() => aScore(0)}>Scorereset</button>
         </div>
+        
+        
       </section>
     </div>
   );
